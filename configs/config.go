334d16953c8ccf5ee5c6a6972a84d94a3fdc97d0
Hamid Gharamohammadi <hamid_ah001@yahoo.com>
2025-03-04 23:43:08 +0330

    feat:add configuration

---
+++ configs/config.go
@@ -0,0 +1,46 @@
+package configs
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	log "github.com/sirupsen/logrus"
+)
+
+type Config struct {
+	App struct {
+		Name     string `yaml:"name" env:"NAME" envDefault:"Blog"`
+		Version  string `yaml:"version" env:"VERSION" envDefault:"Latest"`
+		LogLevel string `yaml:"log_level" env:"LOG_LEVEL" envDefault:"debug"`
+		Env      string `yaml:"env" env:"ENV" envDefault:"development"`
+	} `yaml:"app" env-prefix:"APP_"`
+	Router struct {
+		Address string `yaml:"address" env:"ADDRESS"`
+	} `yaml:"router" env-prefix:"ROUTER_"`
+	DB struct {
+		Mongo struct {
+			HostName   string `yaml:"host_name" env:"HOST_NAME"`
+			Port       string `yaml:"port" env:"PORT"`
+			Username   string `yaml:"username" env:"USERNAME"`
+			Password   string `yaml:"password" env:"PASSWORD"`
+			Database   string `yaml:"database" env:"DATABASE"`
+			AuthSource string `yaml:"auth_source" env:"AUTH_SOURCE"`
+		} `yaml:"mongo" env-prefix:"MONGO_"`
+	} `yaml:"db" env-prefix:"DB_"`
+	Auth struct {
+		JWT struct {
+			SecretKey   string `yaml:"secret_key" env:"SECRET_KEY"`
+			Issuer      string `yaml:"issuer" env:"ISSUER" env-default:"Blog"`
+			ExpiredTime int    `yaml:"expired_time" env:"EXPIRED_TIME" envDefault:"24"` //Hour
+		} `yaml:"jwt" env-prefix:"JWT_"`
+	} `yaml:"auth" env-prefix:"AUTH_"`
+}
+
+func Load(filePath string) (*Config, error) {
+	cfg := &Config{}
+	if err := cleanenv.ReadConfig(filePath, cfg); err != nil {
+		log.Error(err)
+		if err = cleanenv.ReadEnv(cfg); err != nil {
+			return nil, err
+		}
+	}
+	return cfg, nil
+}
--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"blog/configs"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	config *configs.Config
+)
+
+func init() {
+	cfg, err := configs.Load("./configs/config.yaml")
+	if err != nil {
+		log.WithError(err).Fatal("failed to load configurations")
+	}
+	config = cfg
+	logLevel, err := log.ParseLevel(cfg.App.LogLevel)
+	if err != nil {
+		log.WithError(err).Fatal("failed to parse log level")
+	}
+	log.SetLevel(logLevel)
+}
 func main() {
 
 }
